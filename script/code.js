5a0f9a219c9914dd025d4c3f68a081fde1af4808
KeenoFourie <keenofourie614@gmail.com>
2023-05-31 11:18:53 +0200

    question 3 done

--- script/code.js
+++ script/code.js
@@ -1,4 +1,3 @@
-Question 3: Renders each person object in the table using JavaScript
 
 // Question 1: Create an array of objects that each contain the name, surname and age of the people in this group. 9 objects in total.
 // Question 1:
@@ -47,3 +46,57 @@ let person9 = {
     surname: 'Kallis',
     age:22
 }
+
+// Question 3: Renders each person object in the table using JavaScript
+// Question 3:
+
+let display = document.querySelector('#output_display')
+console.log(display);
+
+display.innerHTML = `
+<tr>
+    <td>${person1.name}</td>
+    <td>${person1.surname}</td>
+    <td>${person1.age}</td>
+</tr>
+<tr>
+    <td>${person2.name}</td>
+    <td>${person2.surname}</td>
+    <td>${person2.age}</td>
+</tr>
+<tr>
+    <td>${person3.name}</td>
+    <td>${person3.surname}</td>
+    <td>${person3.age}</td>
+</tr>
+<tr>
+    <td>${person4.name}</td>
+    <td>${person4.surname}</td>
+    <td>${person4.age}</td>
+</tr>
+<tr>
+    <td>${person5.name}</td>
+    <td>${person5.surname}</td>
+    <td>${person5.age}</td>
+</tr>
+<tr>
+    <td>${person6.name}</td>
+    <td>${person6.surname}</td>
+    <td>${person6.age}</td>
+</tr>
+<tr>
+    <td>${person7.name}</td>
+    <td>${person7.surname}</td>
+    <td>${person7.age}</td>
+</tr>
+<tr>
+    <td>${person8.name}</td>
+    <td>${person8.surname}</td>
+    <td>${person8.age}</td>
+</tr>
+<tr>
+    <td>${person9.name}</td>
+    <td>${person9.surname}</td>
+    <td>${person9.age}</td>
+</tr>
+`
