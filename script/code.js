224e570828c5a143b544ea124deaa45d25265641
KeenoFourie <keenofourie614@gmail.com>
2023-05-31 11:02:02 +0200

    question 1 done

---
+++ script/code.js
@@ -0,0 +1,50 @@
+Question 1: Create an array of objects that each contain the name, surname and age of the people in this group. 9 objects in total.
+Question 2: Create a table on your html with headings for "Name", "Surname", and "Age"
+Question 3: Renders each person object in the table using JavaScript
+
+// Question 1:
+let person1 = {
+    name: 'Amy',
+    surname: 'Pieterse',
+    age: 22
+}
+let person2 = {
+    name: 'Mish-Al',
+    surname: 'Taliep',
+    age: 20
+}
+let person3 = {
+    name: 'Jason',
+    surname: 'Viljoen',
+    age: 95
+}
+let person4 = {
+    name: 'Logan',
+    surname: 'Paul',
+    age: 24
+}
+let person5 = {
+    name: 'Jason',
+    surname: 'Barron',
+    age: 35
+}
+let person6 = {
+    name: 'Alonso',
+    surname: 'Cupido',
+    age: 41
+}
+let person7 = {
+    name: 'Siyamthanda',
+    surname: 'Mazokwana',
+    age: 54
+}
+let person8 = {
+    name: 'Keeno',
+    surname: 'Fourie',
+    age: 18
+}
+let person9 = {
+    name: 'Seth',
+    surname: 'Kallis',
+    age:22
+}
